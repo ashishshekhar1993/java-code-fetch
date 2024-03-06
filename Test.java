3bd837194ef44a40fef2d2d5a86c40d751446ce2
Ashish Shekhar <ashishshekhar000@gmail.com>
2024-03-06 12:53:05 +0530

    Added test file

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+	public static void main(string[] args{
+	System.out.println("**************");
+	System.out.println("Github code fetch");
+	System.out.println("**************");
+	})
+}
