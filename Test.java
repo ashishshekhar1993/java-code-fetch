4678ec60730fcd6a8b2b51e6c7b0f521b468c6d0
ashishshekhar1993 <ashishshekhar000@gmail.com>
2024-03-06 13:44:08 +0530

    Code changes

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
-public class Test{
-	public static void main(string[] args{
-	System.out.println("**************");
-	System.out.println("Github code fetch");
-	System.out.println("**************");
-	})
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
 }
